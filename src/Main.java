faef630cd5adf7ecc1a5b606f526cbcea36cf5ca
gaoweipo <gaoweipo@hotmail.com>
2023-02-20 00:15:14 -0500

    Add the algorithm and output content to complete the program writing.

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         final double d=10, h=3, weight=750, Pi=3.14;
         double V, Weight;
-
+        V=Pi*(d/2)*(d/2);
+        Weight=weight*V;
+        System.out.println("The volume is:"+V+"; The total storage weight is:"+Weight);
     }
 }
